1f5204b127880b453aa2f78af29f38b93c5074b3
Matheus Graciano (pc-beta) <graciano.dev@gmail.com>
2017-03-03 03:15:08 -0300

    finishing style from home (pending: push menu)

--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -5,10 +5,11 @@
     @forelse($posts as $post)
     <li>
         <a href="{{ url($post->slug) }}" title="{{ $post->title }}" class="post-card">
-            <section class="post-info">
+            <section>
                 <h2 class="title">{{ $post->title }}</h2>
                 <p class="preview">{{ $post->preview_text }}</p>
                 <img src="{{ $post->cover_image }}" alt="Cover of {{ $post->title }}">
+                <hr>
                 <h3 class="lead">{{ $post->lead }}</h3>
                 <p class="excerpt">{{ $post->excerpt }}</p>
             </section>
