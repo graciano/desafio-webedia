9cc988759544fb9aedd29dfdd1a51a0d12f962af
Matheus Graciano (pc-beta) <graciano.dev@gmail.com>
2017-03-03 04:00:07 -0300

    push menu done

--- resources/views/layouts/content.blade.php
+++ resources/views/layouts/content.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
 <header>
+    <button id="button-menu" class="push-button" alt="Show menu">
+        <span class="strip"></span>
+        <span class="strip"></span>
+        <span class="strip"></span>
+    </button>
     <h2>
         A site from
         <a href="{{ route('home') }}" class="wb-logo">
@@ -9,7 +14,7 @@
         </a>
         group
     </h2>
-    <nav class="nav-menu">
+    <nav class="nav-menu pushmenu pushmenu-left">
         <ul class="nav-list">
             <li><a href="">category</a></li>
             <li><a href="">category</a></li>
