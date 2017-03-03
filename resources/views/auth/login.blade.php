2355487cbce414ea7d7f25fd23fdbec8d0dedcd5
Matheus Graciano (pc-beta) <graciano.dev@gmail.com>
2017-03-03 10:54:05 -0300

    refactoring sass files

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Escolha uma opção de login</h1>
-    <ul>
-    @foreach($drivers as $driver)
-            <li><a href="{{ route('social.login', $driver) }}">{{ $driver }}</a></li>
-    @endforeach
-    </ul>
+    <div class="component">
+        <h1>Choose a login option</h1>
+        <ul>
+        @foreach($drivers as $driver)
+                <li><a href="{{ route('social.login', $driver) }}">{{ $driver }}</a></li>
+        @endforeach
+        </ul>
+    </div>
 @endsection
